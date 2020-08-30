7793bb9b38b6667ddd4016e76327a9e53068b2b7
Bertodemus <roberto@robertorubet.com>
2020-08-30 15:30:49 -0400

    updated comments and got the JS to operate as expected

--- passgen.js
+++ passgen.js
@@ -1,105 +1,90 @@
+//Assignment Code
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
 var passLength = 0;
 var lower = false;
 var upper = false;
 var number = false;
 var special = false;
 
-do {
-    alert("Please select your password length. Length must be between 8 to 128 characters.")
-    passLength = prompt("Select password Length (8 - 128):");
-    passLength = +passLength;
-    console.log(typeof passLength);
-    console.log(passLength);
+// Event listener for the generate button
+generateBtn.addEventListener("click", passUnify);
+
+    // Function passUnify puts all of the functions together
+function passUnify() {
+
+        //this do/while brings in the password length
+    do {
+        alert("Please select your password length. Length must be between 8 to 128 characters.")
+        passLength = prompt("Select password Length (8 - 128):");
+        passLength = +passLength;
+    }
+    while (passLength < 8 || passLength > 128);
+
+        //this do/while initiates the criteria questionnaire
+    do {
+        alert("Please select the criteria for your password. You must choose at least one option.")
+        runQuestionnaire();
+    }
+        //this validates whether or not an option was picked
+    while (lower === false && upper === false && number === false && special === false);
+
+        //this variable and for loop creates the password; then it is passed along to the page
+    var passComplete = '';
+    for (i = 0; i < passLength; i++) {
+        passComplete = passComplete + passGenerate(lower, upper, number, special);
+    }
+    passwordText.value = passComplete;
 }
-while (passLength < 8 || passLength > 128);
-
-do {
-    alert("Please select the criteria for your password. You must choose at least one option.")
-    runQuestionaire();
-}
-while (lower === false && upper === false && number === false && special === false);
-
-// for (i = 0; i < passLength; i++) {
-//     passGenerate(lower, upper, number, special);
-//     var passComplete = passComplete + passHolder;
-// }
-//  console.log(passComplete);
-
-console.log(passGenerate(lower,upper,number,special));
 
 
 // Generate functions
-// The string.fromcharcode came from an idea introduced by Florin Pop
+    // The string.fromcharcode came from an idea introduced by Florin Pop
+    //this function generates a lowercase letter
 function getLowerRan() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-
+    //this function generates an uppercase letter
 function getUpperRan() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
+    //this function generates a number
 function getNumberRan() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
+    //this function generates a special character
 function getSpecialRan() {
     var specialChar = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","=","<",">","/",",","."]
     return specialChar[Math.floor(Math.random() * specialChar.length)];
 }
 
-//Questionaire
-function runQuestionaire() {
-
+//Questionnaire
+function runQuestionnaire() {
+        //these functions ask for the password criteria
     lower = confirm("Do you want lower case character?");
-    console.log(typeof lower);
-    console.log(lower);
-
     upper = confirm("Do you want upper case characters?");
-    console.log(typeof upper);
-    console.log(upper);
-
     number = confirm("Do you want numbers?");
-    console.log(typeof number);
-    console.log(number);
-
     special = confirm("Do you want special symbols?");
-    console.log(typeof special);
-    console.log(special);
 }
 
 //Password Processing
+//this function takes the results of the questionnaire, generates random number for the selected critera, then picks one of generated values
 function passGenerate(lower, upper, number, special) {
     var passArray = [];
     if (lower === true) {
         passArray.push([getLowerRan()]);
-        console.log(passArray[0]);
     }
 
     if (upper === true) {
         passArray.push([getUpperRan()]);
-        console.log(passArray[1]);
     }
 
     if (number === true) {
         passArray.push([getNumberRan()]);
-        console.log(passArray[2]);
     }
 
     if (special === true) {
         passArray.push([getSpecialRan()]);
-        console.log(passArray[3]);
     }
-    console.log(passArray.length);
     return passArray[Math.floor(Math.random() * passArray.length)];
 }
-
-
-
-console.log(getSpecialRan());
-
-
-// The string.fromcharcode came from an idea introduced by Florin Pop
-// console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
-// console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 65));
-// console.log(String.fromCharCode(Math.floor(Math.random() * 10) + 48));
-// console.log(String.fromCharCode(Math.floor(Math.random() * 15) + 33));
