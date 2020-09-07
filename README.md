fed1e5153da700e2b9594d7862c9099d9adb22ea
Bertodemus <roberto@robertorubet.com>
2020-09-07 17:56:24 -0400

    I updated the README file and some of the comments.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Password-Generator
-Password Generator built in Javascript
+# JavaScript: Password Generator
+
+This is a small application that generates a random password based on user-selected criteria. The user has two options when it comes to selecting the criteria for their password. The user can chose to be prompted for their choices or they can manually select the options vis radio buttons. 
+
+## Overview
+
+* Responsive design consists of: `index.html`, `style.css` and `passgen.js`.
+
+* Bootstrap 4 was utilized to create:
+
+   * A responsive layout
+   * Collapsible elements
+
+* Graphics and logo were created by Roberto Rubet
+
+### Bonus
+
+* A `select` option is provided to the user which allows them to manually and dynamically select their password criteria.
+
+
--- passgen.js
+++ passgen.js
@@ -1,4 +1,4 @@
-//Assignment Code
+// Variable assignment code
 var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 var promptButt = document.querySelector("#promptMe");
@@ -8,7 +8,7 @@ var selectCard = document.querySelector("#selectCard");
 // Event listener for the generate button
 generateBtn.addEventListener("click", howtoGenerate);
 
-
+// These functions control enabling and disabling the "prompt" and "select" buttons
 $('#selectCard').on('shown.bs.collapse', function () {
     promptButt.removeAttribute("disabled");
     selectButt.setAttribute("disabled","");
@@ -19,8 +19,8 @@ $('#selectCard').on('hidden.bs.collapse', function () {
     selectButt.removeAttribute("disabled");
 })
 
+// This will initiate generating the password based on whether you want to use prompt or select
 function howtoGenerate() {
-
     if (document.getElementById("promptMe").checked == true) {
         passUnifyPrompted();
     }
@@ -29,11 +29,10 @@ function howtoGenerate() {
     }
 }
 
-
 // Event listener for the Copy button
 copyClip.addEventListener("click", copyPass);
 
-// Function passUnifySelected puts all of the functions together with selected criteria
+// Function passUnifySelected puts all of the functions together with selected criteria via the radio buttons
 function passUnifySelected() {
     var passComplete = '';
     var passLength = document.getElementById("numLength").value;
@@ -43,7 +42,7 @@ function passUnifySelected() {
     var number = document.getElementById("numberCheck").checked;
     var special = document.getElementById("specialCheck").checked;
     var errorCheck = false;
-
+    // Criteria validation
     if (passLength < 8 || passLength > 128) {
         errorCheck = true;
         alert("Please enter a password length that is between 8 - 128 characters long.");
@@ -63,32 +62,33 @@ function passUnifySelected() {
     return;
 }
 
-// Function passUnify puts all of the functions together through prompts
+// Function passUnify puts all of the functions together through the prompts
 function passUnifyPrompted() {
-    //this do/while brings in the password length
+
+// This do/while brings in the password length
 do {
     alert("Please select your password length. Length must be between 8 to 128 characters.")
     passLength = prompt("Select password Length (8 - 128):");
     passLength = +passLength;
-     //this if statement allows the user to break out of the loop if they 'escape' past the initial prompt or select a value of 0
     if (passLength === 0) {
         break;
     }
 }
 while (passLength < 8 || passLength > 128);
-    //this do/while initiates the criteria questionnaire
+
+// This do/while initiates the criteria questionnaire
 do {
-        //This if statement finishes breaking out of the loop if the user were to 'escape' the initial prompt
     if (passLength === 0) {
         break;
     }
     alert("Please select the criteria for your password. You must choose at least one option.")
     runQuestionnaire();
     }
-    //this validates whether or not an option was picked
+    // This validates whether or not an option was picked
 while (lower === false && upper === false && number === false && special === false);
-    //this variable and for loop creates the password; then it is passed along to the page
+
 var passComplete = '';
+
 for (var i = 0; i < passLength; i++) {
     passComplete = passComplete + passGenerate(lower, upper, number, special);
     }
@@ -98,35 +98,35 @@ passwordText.value = passComplete;
 
 // Generate functions
     // The string.fromcharcode came from an idea introduced by Florin Pop
-    //this function generates a lowercase letter
+    // This function generates a lowercase letter
 function getLowerRan() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-    //this function generates an uppercase letter
+    // This function generates an uppercase letter
 function getUpperRan() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-    //this function generates a number
+    // This function generates a number
 function getNumberRan() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-    //this function generates a special character
+    // This function generates a special character
 function getSpecialRan() {
     var specialChar = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","=","<",">","/",",","."]
     return specialChar[Math.floor(Math.random() * specialChar.length)];
 }
 
-//Questionnaire
+// Prompt Questionnaire
 function runQuestionnaire() {
-    //these functions ask for the password criteria
+    // These functions ask for the password criteria
 lower = confirm("Do you want lower case character? (OK = yes, CANCEL = no)");
 upper = confirm("Do you want upper case characters? (OK = yes, CANCEL = no)");
 number = confirm("Do you want numbers? (OK = yes, CANCEL = no)");
 special = confirm("Do you want special symbols? (OK = yes, CANCEL = no)");
 }
 
-//Password Processing
-//this function takes the results of the questionnaire, generates random number for the selected critera, then picks one of generated values
+// Password Processing
+// This function takes the results of the promp questionnaire or selection toggles, generates random number for the selected critera, then picks one of generated values
 function passGenerate(lower, upper, number, special) {
     var passArray = [];
     if (lower === true) {
@@ -147,13 +147,11 @@ function passGenerate(lower, upper, number, special) {
     return passArray[Math.floor(Math.random() * passArray.length)];
 }
 
-//Copying Text to the clipboard
+//Copy password to the clipboard
 function copyPass() {
-
     var copyText = document.getElementById("password");
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("copy");
-  
     alert("Copied the text: " + copyText.value);
-  }
+}
